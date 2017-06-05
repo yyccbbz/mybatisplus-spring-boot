0931e04bb8a79cd6617b8770c482cd27ca7a329e
juancan@163.com <zizou2015>
2017-06-05 18:22:55 +0800

    更新代码

--- src/main/java/com/baomidou/springboot/controller/UserController.java
+++ src/main/java/com/baomidou/springboot/controller/UserController.java
@@ -27,6 +27,59 @@ public class UserController {
 	@Autowired
 	private IUserService userService;
 
+
+	@ApiOperation(value = "获取用户列表", notes = "获取用户列表")
+	@RequestMapping(value = {""}, method = RequestMethod.GET)
+	public List<User> getUserList() {
+		return 	userService.selectList(null);
+	}
+
+	@ApiOperation(value = "分页获取用户列表", notes = "分页获取用户列表")
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public Page<User> getUserListByPage() {
+		return userService.selectPage(new Page<User>(), null);
+	}
+
+
+	@ApiOperation(value = "创建用户", notes = "根据User对象创建用户")
+	@ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public String postUser(@RequestBody User user) {
+		boolean b = userService.insert(user);
+		return "success";
+	}
+
+
+	@ApiOperation(value = "获取用户详细信息", notes = "根据url的id来获取用户详细信息")
+	@ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public User getUser(@PathVariable Long id) {
+		return userService.selectById(id);
+	}
+
+
+	@ApiOperation(value = "更新用户详细信息", notes = "根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long"),
+			@ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+	})
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public String putUser(@PathVariable Long id, @RequestBody User user) {
+		boolean b = userService.updateById(user);
+		return "success";
+	}
+
+
+	@ApiOperation(value = "删除用户", notes = "根据url的id来指定删除对象")
+	@ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public String deleteUser(@PathVariable Long id) {
+		boolean b = userService.deleteById(id);
+		return "success";
+	}
+
+
+
 	/**
 	 * 分页 PAGE
 	 */
@@ -98,56 +151,6 @@ public class UserController {
 		JSONObject result = new JSONObject();
 		result.put("records", userService.selectListBySQL());
 		return result;
-	}
-
-	static Map<Long, User> users = Collections.synchronizedMap(new HashMap<Long, User>());
-
-	@ApiOperation(value="获取用户列表", notes="")
-	@RequestMapping(value={""}, method= RequestMethod.GET)
-	public List<User> getUserList() {
-		List<User> users = this.userService.selectList(null);
-		return users;
-	}
-
-
-	@ApiOperation(value="创建用户", notes="根据User对象创建用户")
-	@ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
-	@RequestMapping(value="", method=RequestMethod.POST)
-	public String postUser(@RequestBody User user) {
-		users.put(user.getId(), user);
-		return "success";
-	}
-
-
-	@ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
-	@ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public User getUser(@PathVariable Long id) {
-		return users.get(id);
-	}
-	@ApiOperation(value="更新用户详细信息", notes="根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
-	@ApiImplicitParams({
-			@ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long"),
-			@ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
-	})
-
-
-	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	public String putUser(@PathVariable Long id, @RequestBody User user) {
-		User u = users.get(id);
-		u.setName(user.getName());
-		u.setAge(user.getAge());
-		users.put(id, u);
-		return "success";
-	}
-
-
-	@ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
-	@ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
-	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
-	public String deleteUser(@PathVariable Long id) {
-		users.remove(id);
-		return "success";
 	}
 
 }
--- src/main/resources/sm.md
+++ src/main/resources/sm.md
@@ -1,6 +1,6 @@
 
 ####访问连接
-http://localhost:8080/user
-http://localhost:8080/swagger-ui.html
+http://localhost:8888/user
+http://localhost:8888/swagger-ui.html
 
-h2 数据库控制台：http://localhost:8080/console
+h2 数据库控制台：http://localhost:8888/console
