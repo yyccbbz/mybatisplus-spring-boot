0c07136aca7d247cd905494dd33f79daee988225
yyccbbz <juancan@163.com>
2017-06-22 01:19:18 +0800

    2017-06-22

--- src/test/java/com/baomidou/springboot/test/AppTest.java
+++ src/test/java/com/baomidou/springboot/test/AppTest.java
@@ -1,5 +1,12 @@
 package com.baomidou.springboot.test;
 
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -10,10 +17,59 @@ package com.baomidou.springboot.test;
  */
 public class AppTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {}
 
+    File file = new File("E:\\Users\\IdeaProjects\\mybatisplus-spring-boot\\src" +
+            "\\test\\java\\com\\baomidou\\springboot\\test\\abc.txt");
 
-    }
+//        File file = new File("E:\\Workspace\\intellij2017\\mp-spring-boot\\src" +
+//                "\\test\\java\\com\\evergrande\\springboot\\test\\loanBatch.txt");
+//`login_name` varchar(30) NOT NULL COMMENT '登录名称',
+        @Test
+        public void testHtml() throws Exception {
+
+            if (file.isFile() && file.exists()) { //判断文件是否存在
+
+                InputStreamReader read = new InputStreamReader(new FileInputStream(file));
+                BufferedReader bufferedReader = new BufferedReader(read);
+                String lineTxt = null;
+
+                while ((lineTxt = bufferedReader.readLine()) != null) {
+                    String[] strs = lineTxt.split(",");
+                    String s1 = strs[0];
+                    String s2 = strs[1];
+
+//                    String field = "{field: '" + s2 + "', title: '" + s1 + "', width: 80, sortable: true},";
+                    String field = "`" + s1 + "` varchar(100) NOT NULL COMMENT '" + s2 + "' ,";
+                    System.err.println(field);
+                }
+
+            }
+
+        }
+
+        @Test
+        public void testField() throws Exception {
+
+            if (file.isFile() && file.exists()) { //判断文件是否存在
+
+                InputStreamReader read = new InputStreamReader(new FileInputStream(file));
+                BufferedReader bufferedReader = new BufferedReader(read);
+                String lineTxt = null;
+
+                while ((lineTxt = bufferedReader.readLine()) != null) {
+                    String[] strs = lineTxt.split(",");
+                    String s1 = strs[0];
+                    String s2 = strs[1];
+
+                    System.err.println("// " + s1);
+                    System.err.println("private String " + s2 + ";");
+                }
+
+            }
+
+        }
+
 
 
 }
--- src/test/resources/db/base.sql
+++ src/test/resources/db/base.sql
@@ -1,19 +1,27 @@
-Name 姓名
-ID number 身份证号
-phone number 手机号
-Staff coding 员工编码
-gender 性别
-age 年龄
-Entry Time 入职时间
-Resignation time 离职时间
-Length of service 服务年限
-company_name 公司名
-In-service status 在职状态
-Loan application serial number 贷款申请流水号
-Loan application date 贷款申请日期
-The stage of the process 所属流程阶段
-Loan status 贷款状态
-Departure Control 离职管控方式
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id`  bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键' ,
+  `customer_name` varchar(100) NOT NULL COMMENT '姓名' ,
+  `id_no` varchar(100) NOT NULL COMMENT '身份证号' ,
+  `phone_no` varchar(100) NOT NULL COMMENT '手机号' ,
+  `staff_no` varchar(100) NOT NULL COMMENT '员工编码' ,
+  `gender` varchar(100) NOT NULL COMMENT '性别' ,
+  `age` varchar(100) NOT NULL COMMENT '年龄' ,
+  `entry_time` varchar(100) NOT NULL COMMENT '入职时间' ,
+  `resignation_time` varchar(100) NOT NULL COMMENT '离职时间' ,
+  `service_years` varchar(100) NOT NULL COMMENT '服务年限' ,
+  `company_name` varchar(100) NOT NULL COMMENT '公司名' ,
+  `inservice_status` varchar(100) NOT NULL COMMENT '在职状态' ,
+  `loan_application_no` varchar(100) NOT NULL COMMENT '贷款申请流水号' ,
+  `loan_application_date` varchar(100) NOT NULL COMMENT '贷款申请日期' ,
+  `process_stage` varchar(100) NOT NULL COMMENT '所属流程阶段' ,
+  `loan_status` varchar(100) NOT NULL COMMENT '贷款状态' ,
+  `departure_control` varchar(100) NOT NULL COMMENT '离职管控方式' ,
+  `create_time`  timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间' ,
+  `update_time`  timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间' ,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='用户表';
+
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
